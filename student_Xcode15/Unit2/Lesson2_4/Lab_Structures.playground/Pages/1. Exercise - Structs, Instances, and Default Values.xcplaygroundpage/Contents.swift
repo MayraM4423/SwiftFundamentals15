cd69948e064926c066387efa13e3e19026463654
Mayra <mendozadelapilam4423@durhamtech.edu>
2024-11-20 19:46:01 -0500

    Completed Lab_Loops

--- student_Xcode15/Unit2/Lesson2_4/Lab_Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_4/Lab_Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,13 @@
  
  Imagine you are creating an app that will monitor location. Create a `GPS` struct with two variable properties, `latitude` and `longitude`, both with default values of 0.0.
  */
-
-
+struct GPS {
+    var latitude = 0.0
+    var longitude = 0.0
+}
 //:  Create a variable instance of `GPS` called `somePlace`. It should be initialized without supplying any arguments. Print out the latitude and longitude of `somePlace`, which should be 0.0 for both.
-
-
+let somePlace = GPS(int: 0.0)
+print(somePlace.int)
 //:  Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
 
 
